00da6637064ee9551155ea71cd3d3aaa880a0c04
Mushfique <mushfique3214@gmail.com>
2024-05-26 01:15:12 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'DatingApp';
+export class AppComponent implements OnInit{
+  title = 'Dating App';
+  users:any;
+
+  constructor(private http: HttpClient){}
+  ngOnInit(): void {
+    this.http.get('https://localhost:7020/api/Users').subscribe({
+        next: Response  =>{
+            this.users=Response;
+        },
+        error: error => console.log(error),
+      }
+    );
+  }
 }
